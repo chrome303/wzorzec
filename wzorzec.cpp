587e104a2646bf0ab79486d0186c8a85f2ebec82
pblpiwnica <paarchacki@gmail.com>
2013-12-03 09:29:55 -0800

    Menu na dobry początek

--- wzorzec.cpp
+++ wzorzec.cpp
@@ -7,9 +7,35 @@
 using namespace std;
 int main(int argc, char** argv) {
 	
-	/* Tutaj bedzie nieziemski kod *\
+    // MENU
 	
+	int menu;
 	
+	cout << "============================\n";
+	cout << "========= M E N U ==========\n";
+	cout << "============================\n";
+	cout << "\n";
+	cout << "Wybierz zadana opcje z menu wyboru" << endl;
+	cout << "\n";
+	cout << "1. Wskazanie wlasnego pliku do przeszukiwania" << endl;
+	cout << "2. Podanie wzorca ciagu znaku do wyszukania" << endl;
+	cout << "3. Zamkniecie programu" << endl;
+	cin >> menu;
+	
+	switch ( menu)
+    	{
+    		case 3:
+    			cout << "Koniec programu" << endl;
+    			break;
+    		case 1:
+    			cout << "Wskaz plik" << endl;
+    			break;
+    		default:
+    			cout << "Blad!!" << endl;
+    			break;
+    	}
+   
+
 	getch();
 	return 0;
 }
